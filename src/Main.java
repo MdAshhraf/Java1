157ddaef31d2f62c8d0de6efa240793e137d5711
MdAshhraf <2221467@iub.edu.bd>
2023-10-07 01:08:26 +0600

    kutta

---
+++ src/Main.java
@@ -0,0 +1,54 @@
+/*
+
+public class Main {
+    public static void main(String[]args){
+        float myFloat = 10.89f;
+        int myInt = (int)myFloat;// Explicitly cast the float to int
+        System.out.println(myInt);
+        System.out.println(myFloat);
+    }
+}
+*/
+
+/*public class Main {
+    public static void main(String[]args){
+        int x = 100;
+        x +=10;
+        System.out.println(x);
+    }
+}*/
+
+import com.sun.security.jgss.GSSUtil;
+
+
+//1 Problem
+/*public class Main {
+    public static void main(String[]args){
+        String First_name = "MD Ashraful";
+        String Last_name = "Islam";
+        *//*System.out.println(First_name.concat(Last_name))*//*;
+        String txt ="MD Ashraful\rIslam";
+        System.out.println(txt);
+    }
+
+}*/
+
+
+/*
+public class Main {
+    public static void main(String[] args) {
+        String First_name = "MD Ashraful";
+        String Last_name = "Islam";
+
+        String txt = "MD Ashraful \f Islam";
+        System.out.println(txt);
+    }
+}*/
+
+
+public class Main {
+    public static void main(String[] args) {
+    String name = "Ashraf";
+        System.out.println(name.indexOf("s"));
+    }
+}
